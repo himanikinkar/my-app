228ec879425d6100930d82b10dfffef82f190f71
Himani <himanikinkar02@gmail.com>
2022-10-15 02:16:37 +0530

    fecthing api data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import Data from './records.json';
 
-function App() {
+function App(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <br />
+      <div className="Header">User Information</div>
+      <div className="upper-box">
+      {Data && Data.map(data => {
+          return (
+             <div className="box" key={data.id}>
+              <div className="Image">
+               <img src={data.image} alt="" width="150px" height="150px"/>
+              </div>
+              {data.name}<br />
+              {data.number}<br />
+
+              {data.tech.map( skill => {
+                return(
+                  <div key={data.id}>
+                  {skill.name}
+                  </div>
+                )
+              }
+
+              )}
+             </div>
+
+          )
+        })
+      }
     </div>
-  );
+    </div>
+  )
 }
 
 export default App;
